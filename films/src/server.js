90579e93ec5ffa6739357d6f69d33a0f77b36eb7
Daniel9822 <dhdany64@gmail.com>
2023-09-12 15:24:13 -0400

    update

--- films/src/server.js
+++ films/src/server.js
@@ -1,5 +1,22 @@
-const server = require("express")();
+const express = require("express");
+const server = express();
 
-server.use(require('./router/films'))
+server.use(express.json());
+
+server.use(require("./router/films"));
+
+server.use("/", (req, res) => {
+    res.status(200).send("hola mundo");
+});
+server.use("*", (req, res) => {
+    res.status(404).send("not found");
+});
+
+server.use((err, req, res, next) => {
+    res.status(err.statusCode || 500).send({
+        error: true,
+        message: err.message,
+    });
+});
 
 module.exports = server;
