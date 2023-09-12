6fb5f7c2ab168a3b7ee1c0ed6d95e40fc6b089d8
Daniel9822 <dhdany64@gmail.com>
2023-09-12 15:24:53 -0400

    update

--- planet/src/server.js
+++ planet/src/server.js
@@ -1,6 +1,18 @@
-const server = require('express')()
+const response = require("./utils/response");
 
+const server = require("express")();
 
-server.use(require('./routes/planet'))
+server.use(require("./routes/planet"));
 
-module.exports = server
+server.use("/", (req, res) => {
+    response(res, 404, "not found");
+});
+
+server.use((err, req, res, next) => {
+    res.status(err.statusCode || 500).send({
+        error: true,
+        message: err.message,
+    });
+});
+
+module.exports = server;
