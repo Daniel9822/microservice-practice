d614e4f52aff02423f786b67683bc4cf5e5280ee
Daniel9822 <dhdany64@gmail.com>
2023-09-12 15:23:07 -0400

    update

--- films/data/index.js
+++ films/data/index.js
@@ -1,7 +1,9 @@
+const { HandleError } = require("../src/utils/errors/handleError");
 const films = require("./films.json");
 
 module.exports = {
     list: async () => {
+        // throw new HandleError('algo salio mal', 500)
         return films;
     },
 };
