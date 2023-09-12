8087ca8564e47d32c22eed1ef7fd6523c18b54be
Daniel9822 <dhdany64@gmail.com>
2023-09-12 15:24:24 -0400

    update

--- planet/data/index.js
+++ planet/data/index.js
@@ -1,7 +1,9 @@
+const { HandleError } = require("../src/utils/errors/handleAsyncErrors");
 const planet = require("./planets.json");
 
 module.exports = {
     list: async () => {
+        throw new HandleError('algo salio mal', 500)
         return planet;
     },
 };
