e95ae206544842442fdd21db003a952e32555309
Daniel9822 <dhdany64@gmail.com>
2023-09-12 15:23:42 -0400

    update

--- films/src/router/films.js
+++ films/src/router/films.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
-const Films = require("../../data");
+const controllers = require('../controller')
 
-router.get("/films", async (req, res) => {
-    const films = await Films.list()
-    res.status(200).json(films);
-});
+
+router.get("/films", controllers.getFilms);
 
 module.exports = router;
