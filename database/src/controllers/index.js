31b85fad7527e2142f05da931a6deb35890a2779
Daniel9822 <dhdany64@gmail.com>
2023-09-20 16:19:43 -0400

    update

--- database/src/controllers/index.js
+++ database/src/controllers/index.js
@@ -1,9 +1,13 @@
 const errorCatcher = require("../helpers/errors/errorCatcher")
 const { insertCharacterCtrl, listCharacterCtrl } = require("./characterCtrl")
 const { insertFilmCtrl, listFilmCtrl } = require("./filmCtrl")
+const { insertPlanetCtrl, listPlanetCtrl } = require("./planetCtrl")
+
 module.exports = {
   insertCharacter: errorCatcher(insertCharacterCtrl),
   listCharacter: errorCatcher(listCharacterCtrl),
   insertFilm: errorCatcher(insertFilmCtrl),
-  listFilm: errorCatcher(listFilmCtrl)
+  listFilm: errorCatcher(listFilmCtrl),
+  insertPlanet: errorCatcher(insertPlanetCtrl),
+  listPlanet: errorCatcher(listPlanetCtrl)
 }
