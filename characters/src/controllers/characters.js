f3a742cb71bc76b251081f93b2d2a3c08ee88cdc
Daniel9822 <dhdany64@gmail.com>
2023-09-20 14:25:55 -0400

    update

--- characters/src/controllers/characters.js
+++ characters/src/controllers/characters.js
@@ -1,12 +1,21 @@
-const response = require("../utils/response");
-const Character = require("../data");
+const response = require("../utils/response")
+const Character = require("../data")
 
 const getCharacter = async (req, res) => {
-    const data = await Character.list()
-    response(res, 200, data);
-};
+  const data = await Character.list()
+  response(res, 200, data)
+}
 
+const createCharacter = async (req, res) => {
+  const body = req.body
+  //TODO validar body;
+  console.log(body)
+
+  const data = await Character.create(body)
+  response(res, 201, data)
+}
 
 module.exports = {
-    getCharacter
+  getCharacter,
+  createCharacter
 }
