6ff442c1e5d5a2ced7e2206b0e24d1f3bb0c83d4
Daniel9822 <dhdany64@gmail.com>
2023-09-20 16:17:33 -0400

    update

--- apiGatway/src/server.js
+++ apiGatway/src/server.js
@@ -1,32 +1,36 @@
-const express = require("express");
-const morgan = require("morgan");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const express = require("express")
+const morgan = require("morgan")
+const { createProxyMiddleware } = require("http-proxy-middleware")
+const proxyReq = require("./utils/proxyReq")
 
-const server = express();
+const server = express()
 
-server.use(express.json());
-server.use(morgan("dev"));
+server.use(express.json())
+server.use(morgan("dev"))
 
 server.use(
-    "/characters",
-    createProxyMiddleware({
-        target: "http://characters:3000",
-        changeOrigin: true,
-    })
-);
+  "/characters",
+  createProxyMiddleware({
+    target: "http://characters:3000",
+    changeOrigin: true,
+    onProxyReq: proxyReq
+  })
+)
 server.use(
-    "/films",
-    createProxyMiddleware({
-        target: "http://films:3001",
-        changeOrigin: true,
-    })
-);
+  "/films",
+  createProxyMiddleware({
+    target: "http://films:3001",
+    changeOrigin: true,
+    onProxyReq: proxyReq
+  })
+)
 server.use(
-    "/planet",
-    createProxyMiddleware({
-        target: "http://planet:3002",
-        changeOrigin: true,
-    })
-);
+  "/planet",
+  createProxyMiddleware({
+    target: "http://planet:3002",
+    changeOrigin: true,
+    onProxyReq: proxyReq
+  })
+)
 
-module.exports = server;
+module.exports = server
