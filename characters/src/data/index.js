644d07158c4f0fe767aea1a2b724bc11e5142279
Daniel9822 <dhdany64@gmail.com>
2023-09-20 14:26:35 -0400

    update

--- characters/src/data/index.js
+++ characters/src/data/index.js
@@ -1,8 +1,25 @@
-const { ResponseError } = require("../utils/errors");
-const characters = require("./characters.json");
+const { DB_SERVICE } = require("../config/envs")
+
+const PATH = `${DB_SERVICE}/characters`
 
 module.exports = {
-    list: async () => {
-        return characters
-    },
-};
+  list: async () => {
+    const res = await fetch(PATH)
+    const { data } = await res.json()
+    return data
+  },
+
+  create: async (character) => {
+    const res = await fetch(PATH, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(character)
+    })
+    const { data } = await res.json()
+    console.log(data)
+
+    return data
+  }
+}
