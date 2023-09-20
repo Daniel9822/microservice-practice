f529ed1d802373c2ced80659fd08129d04c3341b
Daniel9822 <dhdany64@gmail.com>
2023-09-20 14:27:00 -0400

    update

--- characters/src/server.js
+++ characters/src/server.js
@@ -1,22 +1,21 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require("express")
+const morgan = require("morgan")
 
-const server = express();
+const server = express()
 
+server.use(morgan("dev"))
+server.use(express.json())
+server.use(require("./routes/characters"))
 
-server.use(morgan('dev'))
-server.use(require("./routes/characters"));
-
-server.use('*', (req, res) => {
-    return res.status(404).send('Not found')
+server.use("*", (req, res) => {
+  return res.status(404).send("Not found")
 })
 
 server.use((err, req, res, next) => {
-    res.status(err.statusCode || 500).send({
-        error: true,
-        message: err.message
-    })
+  res.status(err.statusCode || 500).send({
+    error: true,
+    message: err.message
+  })
 })
 
-
-module.exports = server;
+module.exports = server
