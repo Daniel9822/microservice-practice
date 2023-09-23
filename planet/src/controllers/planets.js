4f250d0f3ee5e361ffb327f26d1f0ebcbe934add
Daniel9822 <dhdany64@gmail.com>
2023-09-22 22:28:08 -0400

    update

--- planet/src/controllers/planets.js
+++ planet/src/controllers/planets.js
@@ -1,12 +1,21 @@
-const Planet = require("../../data");
-const response = require("../utils/response");
+const Planet = require("../../data")
+const response = require("../utils/response")
 
 const getAllPlanetCtrl = async (req, res) => {
-    const data = await Planet.list();
-    response(res, 200, data);
-};
+  console.log(req.url)
 
+  const data = await Planet.list()
+  response(res, 200, data)
+}
+
+const createPlanetCtrl = async (req, res) => {
+  const body = req.body
+  const data = await Planet.insert(body)
+
+  response(res, 201, data)
+}
 
 module.exports = {
-    getAllPlanetCtrl
+  getAllPlanetCtrl,
+  createPlanetCtrl
 }
