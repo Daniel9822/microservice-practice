ebfff9b575c0a64dd61e637e5938441212d0a7a4
Daniel9822 <dhdany64@gmail.com>
2023-09-22 22:26:20 -0400

    update

--- films/src/controller/films.js
+++ films/src/controller/films.js
@@ -1,11 +1,18 @@
 const Films = require("../../data")
 const response = require("../utils/response")
 
-const getFilms = async (req, res) => {
-    const films = await Films.list()
-    response(res, 200, films)
+const getFilmsCtrl = async (req, res) => {
+  const films = await Films.list()
+  response(res, 200, films)
+}
+
+const createFilmsCtrl = async (req, res) => {
+  const body = req.body
+  const data = await Films.insert(body)
+  response(res, 201, data)
 }
 
 module.exports = {
-    getFilms
+  getFilmsCtrl,
+  createFilmsCtrl
 }
