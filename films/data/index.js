96aa974c9cc24316ddeddb80aa6c497203872e03
Daniel9822 <dhdany64@gmail.com>
2023-09-22 22:25:27 -0400

    update

--- films/data/index.js
+++ films/data/index.js
@@ -1,9 +1,25 @@
-const { HandleError } = require("../src/utils/errors/handleError");
-const films = require("./films.json");
+const { DATABASE_SERVICE } = require("../src/config/envs")
+const { HandleError } = require("../src/utils/errors/handleError")
+const films = require("./films.json")
+
+const PATH = DATABASE_SERVICE
 
 module.exports = {
-    list: async () => {
-        // throw new HandleError('algo salio mal', 500)
-        return films;
-    },
-};
+  list: async () => {
+    const res = await fetch(PATH)
+    const { data } = await res.json()
+    return data
+  },
+
+  insert: async (film) => {
+    const res = await fetch(PATH, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(film)
+    })
+    const { data } = await res.json()
+    return data
+  }
+}
