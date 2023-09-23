2e3733d8a05977970ec8d19dc87833857cd4ab35
Daniel9822 <dhdany64@gmail.com>
2023-09-22 22:28:23 -0400

    update

--- planet/src/routes/planet.js
+++ planet/src/routes/planet.js
@@ -1,6 +1,7 @@
-const router = require("express").Router();
-const controllers = require('../controllers')
+const router = require("express").Router()
+const controllers = require("../controllers")
 
-router.get("/planet", controllers.getAllPlanet);
+router.get("/planet", controllers.getAllPlanet)
+router.post("/planet", controllers.createPlanet)
 
-module.exports = router;
+module.exports = router
