3b730009ba0071b791cd97c9fc122872ccaad3ab
Daniel9822 <dhdany64@gmail.com>
2023-09-30 22:43:03 -0400

    update

--- films/src/router/films.js
+++ films/src/router/films.js
@@ -1,7 +1,12 @@
-const router = require("express").Router()
-const controllers = require("../controller")
+const router = require("express").Router();
+const controllers = require("../controller");
+const {
+  filmCreateValidator,
+  filmsValidationParam
+} = require("../validator/filmCreate.validator");
 
-router.get("/films", controllers.getFilms)
-router.post("/films", controllers.createFilm)
+router.get("/films", controllers.getFilms);
+router.post("/films", filmCreateValidator, controllers.createFilm);
+router.delete("/films/:id", filmsValidationParam, controllers.removeFilm);
 
-module.exports = router
+module.exports = router;
