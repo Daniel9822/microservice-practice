a4d5e608beed63320d8a19acd30ead87e9af1c1e
Daniel9822 <dhdany64@gmail.com>
2023-09-30 22:39:10 -0400

    update

--- database/src/controllers/characterCtrl.js
+++ database/src/controllers/characterCtrl.js
@@ -1,23 +1,33 @@
-const response = require("../helpers/response")
-const modelsNames = require("../helpers/modelsName")
-const { listService, insertServices } = require("../services")
+const response = require("../helpers/response");
+const modelsNames = require("../helpers/modelsName");
+const { listService, insertServices, removeService } = require("../services");
 
 const listCharacterCtrl = async (req, res) => {
-  const model = modelsNames.characterModel
-  const data = await listService({ model })
-  response(res, 200, data)
-}
+  const model = modelsNames.characterModel;
+  const data = await listService({ model });
+  console.log(data);
+
+  response(res, 200, data);
+};
 
 const insertCharacterCtrl = async (req, res) => {
-  const body = req.body
-  const model = modelsNames.characterModel
-  console.log(body)
+  const body = req.body;
+  const model = modelsNames.characterModel;
+  console.log(body);
 
-  const data = await insertServices({ model, data: body })
-  response(res, 200, data)
-}
+  const data = await insertServices({ model, data: body });
+  response(res, 200, data);
+};
+
+const removeCharacterCtrl = async (req, res) => {
+  const { id } = req.params;
+  const model = modelsNames.characterModel;
+  const data = await removeService({ model, id });
+  response(res, 200, data);
+};
 
 module.exports = {
   listCharacterCtrl,
-  insertCharacterCtrl
-}
+  insertCharacterCtrl,
+  removeCharacterCtrl
+};
