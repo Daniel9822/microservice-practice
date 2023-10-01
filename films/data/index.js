f2105dc65a8111fb4ba7d01f12b45d0bab0e08b6
Daniel9822 <dhdany64@gmail.com>
2023-09-30 22:41:37 -0400

    update

--- films/data/index.js
+++ films/data/index.js
@@ -1,14 +1,14 @@
-const { DATABASE_SERVICE } = require("../src/config/envs")
-const { HandleError } = require("../src/utils/errors/handleError")
-const films = require("./films.json")
+const { DATABASE_SERVICE } = require("../src/config/envs");
+const films = require("./films.json");
 
-const PATH = DATABASE_SERVICE
+const PATH = DATABASE_SERVICE;
 
 module.exports = {
   list: async () => {
-    const res = await fetch(PATH)
-    const { data } = await res.json()
-    return data
+    const res = await fetch(PATH);
+    const { data } = await res.json();
+
+    return data;
   },
 
   insert: async (film) => {
@@ -18,8 +18,17 @@ module.exports = {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(film)
-    })
-    const { data } = await res.json()
-    return data
+    });
+    const { data } = await res.json();
+    return data;
+  },
+
+  removeFilm: async (id) => {
+    const res = await fetch(`${PATH}/${id}`, {
+      method: "DELETE"
+    });
+
+    const { data } = await res.json();
+    return data;
   }
-}
+};
