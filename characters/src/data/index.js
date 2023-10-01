80167ac3f22348223025b3a4af7248af6fffc293
Daniel9822 <dhdany64@gmail.com>
2023-09-30 22:38:41 -0400

    update

--- characters/src/data/index.js
+++ characters/src/data/index.js
@@ -1,12 +1,12 @@
-const { DB_SERVICE } = require("../config/envs")
+const { DB_SERVICE } = require("../config/envs");
 
-const PATH = `${DB_SERVICE}/characters`
+const PATH = `${DB_SERVICE}/characters`;
 
 module.exports = {
   list: async () => {
-    const res = await fetch(PATH)
-    const { data } = await res.json()
-    return data
+    const res = await fetch(PATH);
+    const { data } = await res.json();
+    return data;
   },
 
   create: async (character) => {
@@ -16,10 +16,19 @@ module.exports = {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(character)
-    })
-    const { data } = await res.json()
-    console.log(data)
+    });
+    const { data } = await res.json();
+    console.log(data);
 
-    return data
+    return data;
+  },
+
+  deleteChar: async (id) => {
+    const res = await fetch(`${PATH}/${id}`, {
+      method: "DELETE"
+    });
+
+    const { data } = await res.json();
+    return data;
   }
-}
+};
