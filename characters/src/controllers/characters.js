a29e63893ba6e75ce314fa0b4efd4ccbb50a4e23
Daniel9822 <dhdany64@gmail.com>
2023-09-30 22:37:54 -0400

    update

--- characters/src/controllers/characters.js
+++ characters/src/controllers/characters.js
@@ -1,21 +1,29 @@
-const response = require("../utils/response")
-const Character = require("../data")
+const response = require("../utils/response");
+const Character = require("../data");
+const { matchedData } = require("express-validator");
 
 const getCharacter = async (req, res) => {
-  const data = await Character.list()
-  response(res, 200, data)
-}
+  const data = await Character.list();
+  response(res, 200, data);
+};
 
 const createCharacter = async (req, res) => {
-  const body = req.body
-  //TODO validar body;
-  console.log(body)
+  const body = matchedData(req);
+  console.log(body);
 
-  const data = await Character.create(body)
-  response(res, 201, data)
-}
+  const data = await Character.create(body);
+  response(res, 201, data);
+};
+
+const deleteCharacter = async (req, res) => {
+  const { id } = req.params;
+
+  const removeCharacter = await Character.deleteChar(id);
+  response(res, 200, removeCharacter);
+};
 
 module.exports = {
   getCharacter,
-  createCharacter
-}
+  createCharacter,
+  deleteCharacter
+};
