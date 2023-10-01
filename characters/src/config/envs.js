98f774fe39240698b17f51b2d17ecf5f0d38f67f
Daniel9822 <dhdany64@gmail.com>
2023-09-30 22:37:41 -0400

    update

--- characters/src/config/envs.js
+++ characters/src/config/envs.js
@@ -1,5 +1,12 @@
-require("dotenv").config()
+require("dotenv").config();
+
+const NODE_ENV = process.env.NODE_ENV;
+
+const DB_SERVICE =
+  NODE_ENV === "development"
+    ? process.env.DATABASE_SERVICE_DEV
+    : process.env.DATABASE_SERVICE_PROD;
 
 module.exports = {
-  DB_SERVICE: process.env.DATABASE_SERVICE
-}
+  DB_SERVICE
+};
