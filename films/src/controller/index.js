5d12c0b00d101c2149ae29b5727c0d2ee95f8fd4
Daniel9822 <dhdany64@gmail.com>
2023-09-30 22:42:49 -0400

    update

--- films/src/controller/index.js
+++ films/src/controller/index.js
@@ -1,7 +1,8 @@
-const errorCatcher = require("../utils/errorCatcher")
-const { getFilmsCtrl, createFilmsCtrl } = require("./films")
+const errorCatcher = require("../utils/errorCatcher");
+const { getFilmsCtrl, createFilmsCtrl, removeFilm } = require("./films");
 
 module.exports = {
   getFilms: errorCatcher(getFilmsCtrl),
-  createFilm: errorCatcher(createFilmsCtrl)
-}
+  createFilm: errorCatcher(createFilmsCtrl),
+  removeFilm: errorCatcher(removeFilm)
+};
