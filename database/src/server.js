0f90e5271576f6c39b7f44e2e9bbd41c5064f101
Daniel9822 <dhdany64@gmail.com>
2023-09-30 22:41:14 -0400

    update

--- database/src/server.js
+++ database/src/server.js
@@ -1,17 +1,19 @@
-const express = require("express")
-const morgan = require("morgan")
+const express = require("express");
+const morgan = require("morgan");
 
-const server = express()
+const server = express();
 
-server.use(morgan("dev"))
-server.use(express.json())
-server.use("/database", require("./routes"))
+server.use(morgan("dev"));
+server.use(express.json());
+server.use("/database", require("./routes"));
 
 server.use((err, req, res, next) => {
   res.status(err?.statusCode || 500).send({
     error: true,
     message: err.message
-  })
-})
+  });
 
-module.exports = server
+  console.log(err);
+});
+
+module.exports = server;
