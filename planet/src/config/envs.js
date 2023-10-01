f36e133f3ffc7c59049d48790f0a80b025870840
Daniel9822 <dhdany64@gmail.com>
2023-09-30 22:43:51 -0400

    update

--- planet/src/config/envs.js
+++ planet/src/config/envs.js
@@ -1,5 +1,12 @@
-require("dotenv").config()
+require("dotenv").config();
+
+const NODE_ENV = process.env.NODE_ENV;
+
+const DATABASE_SERVICE =
+  NODE_ENV === "development"
+    ? process.env.DATABASE_SERVICE_DEV
+    : process.env.DATABASE_SERVICE_PROD;
 
 module.exports = {
-  DATABASE_SERVICE: process.env.DATABASE_SERVICE
-}
+  DATABASE_SERVICE
+};
