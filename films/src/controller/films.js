9b4e326f6df55cfd2b6bb0f4f5b84472b8a102a7
Daniel9822 <dhdany64@gmail.com>
2023-09-30 22:42:38 -0400

    update

--- films/src/controller/films.js
+++ films/src/controller/films.js
@@ -1,18 +1,26 @@
-const Films = require("../../data")
-const response = require("../utils/response")
+const Films = require("../../data");
+const response = require("../utils/response");
+const { matchedData } = require("express-validator");
 
 const getFilmsCtrl = async (req, res) => {
-  const films = await Films.list()
-  response(res, 200, films)
-}
+  const films = await Films.list();
+  response(res, 200, films);
+};
 
 const createFilmsCtrl = async (req, res) => {
-  const body = req.body
-  const data = await Films.insert(body)
-  response(res, 201, data)
-}
+  const body = matchedData(req);
+  const data = await Films.insert(body);
+  response(res, 201, data);
+};
+
+const removeFilm = async (req, res) => {
+  const { id } = req.params;
+  const data = await Films.removeFilm(id);
+  response(res, 200, data);
+};
 
 module.exports = {
   getFilmsCtrl,
-  createFilmsCtrl
-}
+  createFilmsCtrl,
+  removeFilm
+};
