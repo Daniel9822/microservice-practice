f9116d278adeac9a66ef3882a06607aa2618cbfa
Daniel9822 <dhdany64@gmail.com>
2023-09-30 22:39:47 -0400

    update

--- database/src/controllers/planetCtrl.js
+++ database/src/controllers/planetCtrl.js
@@ -1,22 +1,29 @@
-const modelsName = require("../helpers/modelsName")
-const response = require("../helpers/response")
-const { listService, insertServices } = require("../services")
+const modelsName = require("../helpers/modelsName");
+const response = require("../helpers/response");
+const { listService, insertServices, removeService } = require("../services");
 
 const listPlanetCtrl = async (req, res) => {
-  const model = modelsName.planetModel
-  const data = await listService({ model })
-  response(res, 200, data)
-}
+  const model = modelsName.planetModel;
+  const data = await listService({ model });
+  response(res, 200, data);
+};
 
 const insertPlanetCtrl = async (req, res) => {
-  const body = req.body
-  //TODO validar datos del body
-  const model = modelsName.planetModel
-  const data = await insertServices({ model, data: body })
-  response(res, 201, data)
-}
+  const body = req.body;
+  const model = modelsName.planetModel;
+  const data = await insertServices({ model, data: body });
+  response(res, 201, data);
+};
+
+const removePlanetCtrl = async (req, res) => {
+  const { id } = req.params;
+  const model = modelsName.planetModel;
+  const data = await removeService({ model, id });
+  response(res, 200, data);
+};
 
 module.exports = {
   listPlanetCtrl,
-  insertPlanetCtrl
-}
+  insertPlanetCtrl,
+  removePlanetCtrl
+};
