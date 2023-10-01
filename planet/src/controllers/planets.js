27036a863d70c538ad96ed8a1f9203f4832d8f65
Daniel9822 <dhdany64@gmail.com>
2023-09-30 22:44:18 -0400

    update

--- planet/src/controllers/planets.js
+++ planet/src/controllers/planets.js
@@ -1,21 +1,26 @@
-const Planet = require("../../data")
-const response = require("../utils/response")
+const Planet = require("../../data");
+const response = require("../utils/response");
+const { matchedData } = require("express-validator");
 
 const getAllPlanetCtrl = async (req, res) => {
-  console.log(req.url)
-
-  const data = await Planet.list()
-  response(res, 200, data)
-}
+  const data = await Planet.list();
+  response(res, 200, data);
+};
 
 const createPlanetCtrl = async (req, res) => {
-  const body = req.body
-  const data = await Planet.insert(body)
+  const body = matchedData(req);
+  const data = await Planet.insert(body);
+  response(res, 201, data);
+};
 
-  response(res, 201, data)
-}
+const removePlanetCtrl = async (req, res) => {
+  const { id } = req.params;
+  const data = await Planet.removePlanet(id);
+  response(res, 200, data);
+};
 
 module.exports = {
   getAllPlanetCtrl,
-  createPlanetCtrl
-}
+  createPlanetCtrl,
+  removePlanetCtrl
+};
