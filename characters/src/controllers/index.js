54b3d39f7686b515a17c39884b8f3958f6479132
Daniel9822 <dhdany64@gmail.com>
2023-12-27 16:26:58 -0400

    update

--- characters/src/controllers/index.js
+++ characters/src/controllers/index.js
@@ -2,11 +2,13 @@ const handleError = require("../utils/handleError");
 const {
   getCharacter,
   createCharacter,
-  deleteCharacter
+  deleteCharacter,
+  updateCharacter
 } = require("./characters");
 
 module.exports = {
   getCharacters: handleError(getCharacter),
   createCharacter: handleError(createCharacter),
-  deleteCharacter: handleError(deleteCharacter)
+  deleteCharacter: handleError(deleteCharacter),
+  updateCharacter: handleError(updateCharacter)
 };
