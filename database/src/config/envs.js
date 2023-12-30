7ee539eb4274f04764809dff361f253d5bb45e9a
Daniel9822 <dhdany64@gmail.com>
2023-12-30 18:01:32 -0400

    update

--- database/src/config/envs.js
+++ database/src/config/envs.js
@@ -1,5 +1,14 @@
-require("dotenv").config()
+require("dotenv").config();
+
+const NODE_ENV = process.env.NODE_ENV;
+
+let MONGO_URI = process.env.MONGO_URI;
+
+if (NODE_ENV !== "production") {
+  MONGO_URI = process.env.MONGO_TEST;
+}
 
 module.exports = {
-  MONGO_URI: process.env.MONGO_URI
-}
+  MONGO_URI,
+  NODE_ENV
+};
