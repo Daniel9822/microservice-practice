f78cfcfc465167e6b8b91c64efb4d7b7f9a8353a
Daniel9822 <dhdany64@gmail.com>
2023-12-30 18:00:20 -0400

    update

--- database/index.js
+++ database/index.js
@@ -1,8 +1,17 @@
-const server = require("./src/server")
-const conn = require("./src/config/conn")
+const server = require("./src/server");
+const conn = require("./src/config/conn");
+const { NODE_ENV } = require("./src/config/envs");
 
-const PORT = process.env.PORT || 8004
-server.listen(PORT, async () => {
-  conn()
-  console.log(`Server listener on port ${PORT}`)
-})
+const PORT = process.env.PORT || 8004;
+
+console.log({ NODE_ENV });
+
+if (NODE_ENV !== "test") {
+  server.listen(PORT, async () => {
+    console.log(`Server listener on port ${PORT}`);
+  });
+}
+
+conn();
+
+module.exports = server;
