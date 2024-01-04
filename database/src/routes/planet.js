a7c667c72eb27d5acc1ebba6f39c74fbfe617774
Daniel9822 <dhdany64@gmail.com>
2024-01-03 22:58:11 -0400

    update

--- database/src/routes/planet.js
+++ database/src/routes/planet.js
@@ -5,5 +5,6 @@ const router = require("express").Router();
 router.get("/", planets.listPlanet);
 router.post("/", planets.insertPlanet);
 router.delete("/:id", planets.removePlanet);
+router.put("/:id", planets.updatePlanet);
 
 module.exports = router;
