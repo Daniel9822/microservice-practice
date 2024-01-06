1e1d822ca330fd524b47aa2dc01239bceef89cce
Daniel9822 <dhdany64@gmail.com>
2024-01-05 21:21:33 -0400

    update

--- films/src/config/envs.js
+++ films/src/config/envs.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 
 const NODE_ENV = process.env.NODE_ENV;
 
-const DATABASE_SERVICE = process.env.DATABASE_SERVICE_PROD;
+// const DATABASE_SERVICE = process.env.DATABASE_SERVICE_PROD;
 
-/**const DATABASE_SERVICE =
-  NODE_ENV !== "development"
-    ? process.env.DATABASE_SERVICE_PROD
-    : process.env.DATABASE_SERVICE_DEV; */
+const DATABASE_SERVICE =
+  NODE_ENV !== "production"
+    ? process.env.DATABASE_SERVICE_DEV
+    : process.env.DATABASE_SERVICE_PROD;
 
 console.log(DATABASE_SERVICE);
 
