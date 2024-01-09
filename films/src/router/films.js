dcfa1a8d92674def0ba6931296d4c248e39fa5ee
Daniel9822 <dhdany64@gmail.com>
2024-01-09 11:16:45 -0400

    update

--- films/src/router/films.js
+++ films/src/router/films.js
@@ -8,5 +8,6 @@ const {
 router.get("/films", controllers.getFilms);
 router.post("/films", filmCreateValidator, controllers.createFilm);
 router.delete("/films/:id", filmsValidationParam, controllers.removeFilm);
+router.put("/films/:id", filmsValidationParam, controllers.updateFilm);
 
 module.exports = router;
