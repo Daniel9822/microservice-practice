a037edcc01c1b94aa29e8a68d199d166d55d15c8
Daniel9822 <dhdany64@gmail.com>
2024-01-10 20:30:18 -0400

    update

--- planet/src/controllers/index.js
+++ planet/src/controllers/index.js
@@ -2,11 +2,13 @@ const errorCatcher = require("../utils/errorCatcher");
 const {
   getAllPlanetCtrl,
   createPlanetCtrl,
-  removePlanetCtrl
+  removePlanetCtrl,
+  updatePlanetCtrl
 } = require("./planets");
 
 module.exports = {
   getAllPlanet: errorCatcher(getAllPlanetCtrl),
   createPlanet: errorCatcher(createPlanetCtrl),
-  removePlanet: errorCatcher(removePlanetCtrl)
+  removePlanet: errorCatcher(removePlanetCtrl),
+  updatePlanet: errorCatcher(updatePlanetCtrl)
 };
